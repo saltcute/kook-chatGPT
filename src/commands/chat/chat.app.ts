1f936aa096261f9d097cea996c279d7fb4648a96
Hexona <xuanjiap@lolicon.ac.cn>
2022-12-10 14:40:59 -0500

    refreh toke

--- src/commands/chat/chat.app.ts
+++ src/commands/chat/chat.app.ts
@@ -1,4 +1,3 @@
-import auth from 'configs/auth';
 import { bot } from 'init/client';
 import { AppCommand, AppFunc, BaseSession, Card } from 'kbotify';
 import * as conv from './conversation';
@@ -13,71 +12,7 @@ class Chat extends AppCommand {
         if (session.args.length == 0) {
             return session.reply("No input");
         } else {
-            function getCard(str: string, finished: boolean): Card {
-                return new Card()
-                    .setSize("lg")
-                    .setTheme("info")
-                    .addModule(
-                        {
-                            "type": "context",
-                            "elements": [
-                                {
-                                    "type": "image",
-                                    "src": session.user.avatar
-                                },
-                                {
-                                    "type": "plain-text",
-                                    "content": session.args.join(" ")
-                                }
-                            ]
-                        })
-                    .addDivider()
-                    .addModule(
-                        {
-                            "type": "context",
-                            "elements": [
-                                {
-                                    "type": "image",
-                                    "src": "https://img.kaiheila.cn/assets/2022-12/g6BYoMU88i0f60f4.png"
-                                },
-                                {
-                                    "type": "plain-text",
-                                    "content": `${finished ? `Chat is typing${(Math.trunc(Date.now() / 1000)) % 3 == 0 ? "." : ((Math.trunc(Date.now() / 1000)) % 3 == 1 ? ".." : "...")}` : `Chat`}`
-                                }
-                            ]
-                        })
-                    .addText(str);
-            }
-            const action = async (val: any) => {
-                const initalCard = new Card().setSize("sm").setTheme("warning").addText("Waiting for response...");
-                const messageId = <string>((await session.replyCard(initalCard)).msgSent?.msgId);
-                const chatgpt = new val.ChatGPTAPI({
-                    sessionToken: auth.openAIKey
-                });
-                var string = "";
-                var lastUpdate = 0;
-                await chatgpt.ensureAuth();
-                if (!conv.getConversation(session.channel.id, session.user.id)) {
-                    conv.resetConversation(session.channel.id, session.user.id, chatgpt.getConversation());
-                }
-                conv.getConversation(session.channel.id, session.user.id).sendMessage(session.args.join(" "), {
-                    onProgress: (res: string) => {
-                        string = res;
-                        if (Math.trunc(Date.now() / 400) != lastUpdate) {
-                            bot.API.message.update(messageId, getCard(string, true).toString()).catch((err) => {
-                                console.log(getCard(string, true));
-                                bot.logger.error(err);
-                            });
-                            lastUpdate = Math.trunc(Date.now() / 400);
-                            // console.log(string);
-                        }
-                    }
-                }).then(() => {
-                    bot.logger.info("Done");
-                    setTimeout(() => { bot.API.message.update(messageId, getCard(string, false).toString()); }, 1000);
-                })
-            }
-            eval(`import("chatgpt").then(action)`);
+            conv.run(session);
         }
     };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { refreshToken } from 'commands/chat/conversation';
 import { reset } from 'commands/chat/reset.app';
 import { bot } from 'init/client';
 import { chat } from './commands/chat/chat.app';
@@ -7,6 +8,8 @@ bot.logger.addStream({ level: bot.logger.INFO, stream: process.stdout });
 // bot.logger.addStream({ level: bot.logger.DEBUG, stream: process.stdout }); // DEBUG
 bot.logger.info("Initialization: kook-chatGPT initialization start");
 bot.addCommands(chat, reset);
+
+setInterval(refreshToken, 30 * 60 * 1000);
 
 bot.connect();
 
